d84d669800d8d1a77a04a47b49561e3d5f06bd1a
ash1eygrace <ashleygrobicheau@gmail.com>
2023-01-31 06:36:45 -0700

    create stopwatch component and import into header

---
+++ src/components/Stopwatch.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Stopwatch = () => {
+    return (
+        <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <span className="stopwatch-time">0</span>
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+        </div>
+    );
+
+}
+
+export default Stopwatch;
